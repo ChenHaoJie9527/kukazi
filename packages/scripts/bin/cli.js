027451dfae7c645830f88aa5c751071f4dae206a
ChenHaoJie9527 <a18520145493@163.com>
2024-07-15 23:35:47 +0800

    pnpm optimize-svg 批量优化svg

--- packages/scripts/bin/cli.js
+++ packages/scripts/bin/cli.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
-import { svgTasks } from '../src';
+const { svgTasks } = require('../src');
 
 const [,, command, ...args] = process.argv;
 
 switch (command) {
   case 'optimize-svg':
-    svgTasks.optimizeSvgsInDirectory(args[0] || process.cwd());
+    svgTasks.optimizeSvgsInDirectory(args[0] || process.cwd()).catch(console.error);
     break;
   case 'generate-svg-index':
-    svgTasks.generateIndex(args[0] || process.cwd());
+    svgTasks.generateIndex(args[0] || process.cwd()).catch(console.error);
     break;
   default:
     console.error('Unknown command:', command);

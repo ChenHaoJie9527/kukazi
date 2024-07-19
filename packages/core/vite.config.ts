03006d2770a72fa8856ff140e0749ef8ddfb131d
ChenHaoJie9527 <a18520145493@163.com>
2024-07-19 16:05:52 +0800

    core/vite.config

---
+++ packages/core/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite"
+import path from "path"
+
+export default defineConfig({
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, "src/index.ts"),
+            name: "kukazi-core",
+            fileName(format, entryName) {
+                return `kukazi-core.${format}.js`
+            },
+        },
+        rollupOptions: {
+            external: ["react", "react-dom"],
+            output: {
+                globals: {
+                    react: "React",
+                    "react-dom": "ReactDOM",
+                },
+            },
+        }
+    },
+})

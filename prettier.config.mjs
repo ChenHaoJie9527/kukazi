89aa1734faecfdb5c184063fe53b8422cc98ea4c
ChenHaoJie9527 <a18520145493@163.com>
2024-07-23 22:04:12 +0800

    style: prettier config

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+// prettier.config.mjs
+export default {
+  singleQuote: true,
+  trailingComma: 'es5',
+  printWidth: 100,
+  tabWidth: 2,
+  semi: true,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+};

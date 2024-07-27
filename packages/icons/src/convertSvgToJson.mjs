d30d548f132048f0f9be2fae4fab62a39192fdfc
ChenHaoJie9527 <a18520145493@163.com>
2024-07-27 16:06:54 +0800

    feat: Icons Implementation of Svg to json Data Conversion Scripts

--- packages/icons/src/convertSvgToJson.mjs
+++ packages/icons/src/convertSvgToJson.mjs
@@ -1,11 +1,7 @@
-// icons/scripts/convertSvgToJson.ts
 
-// const path = require('path');
 import path from 'path';
-// const fs = require('fs/promises');
 import fs from 'fs/promises';
-// import { svgToJsonConverter } from '@kukazi/core';
-// import { parserSVG } from 'next-svg-parser';
+import { parserSVG } from 'next-svg-parser';
 
 const SVG_DIR = path.resolve(process.cwd(), 'src/assets');
 const JSON_DIR = path.resolve(process.cwd(), 'src/json');
@@ -13,7 +9,8 @@ async function convertSvgFilesToJson() {
   try {
     // 确保 JSON 目录存在
     await fs.mkdir(JSON_DIR, { recursive: true });
-
+    // 清空 json 目录
+    await clearDirectory()
     // 读取所有 SVG 文件
     const files = await fs.readdir(SVG_DIR);
     const svgFiles = files.filter(file => path.extname(file).toLowerCase() === '.svg');
@@ -23,21 +20,25 @@ async function convertSvgFilesToJson() {
       // 读取 SVG 文件内容
       const svgContent = await fs.readFile(svgPath, 'utf-8');
       // 转换 SVG 到 JSON
-      const iconName = path.basename(svgFile, '.svg');
-      const relativePath = path.relative(process.cwd(), svgPath);
-      // const result = parserSVG(svgContent);
-      // console.log('result =>', result);
-      // const jsonData = svgToJsonConverter({ svgContent, iconName, relativePath });
-
+      // const iconName = path.basename(svgFile, '.svg');
+      // const relativePath = path.relative(process.cwd(), svgPath);
+      const jsonData = parserSVG(svgContent);
       // // 写入 JSON 文件
-      // await fs.writeFile(jsonPath, JSON.stringify(jsonData, null, 2));
+      await fs.writeFile(jsonPath, JSON.stringify(jsonData, null, 2));
 
-      // console.log(`Converted ${svgFile} to JSON`);
+      console.log(`Converted ${svgFile} to JSON`);
     }
 
     console.log('All SVG files have been converted to JSON');
   } catch (error) {
     console.error('Error converting SVG files to JSON:', error);
+  }
+}
+
+async function clearDirectory() {
+  const files = await fs.readdir(JSON_DIR)
+  for (const file of files) {
+    await fs.unlink(path.join(JSON_DIR, file))
   }
 }
 

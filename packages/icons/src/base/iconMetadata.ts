f59678917998cee2e952b78c35eb4693aaf030a6
ChenHaoJie9527 <a18520145493@163.com>
2024-07-30 18:27:20 +0800

    feat: 模拟路径测试

--- packages/icons/src/base/iconMetadata.ts
+++ packages/icons/src/base/iconMetadata.ts
@@ -4,13 +4,14 @@ import { IconMetaData } from "./types"
 
 export class IconMetaDataManager {
   private static cache: Record<string, IconMetaData> = {}
-  private static readonly JSON_DIR = join(__dirname, "..", 'json')
+  private static readonly JSON_DIR = join(__dirname, '..', '/json')
 
   static load(name: string) {
     if (this.cache[name]) {
       return this.cache[name]
     }
-    const jsonContent = readFileSync(join(this.JSON_DIR, `${name}.json`), 'utf-8')
+    const jsonPath = join(this.JSON_DIR, `${name}.json`);
+    const jsonContent = readFileSync(jsonPath, 'utf-8');
     const metaData: IconMetaData = JSON.parse(jsonContent)
     this.cache[name] = metaData
     return metaData
--- packages/icons/src/tests/baseIcon.test.ts
+++ packages/icons/src/tests/baseIcon.test.ts
@@ -1,33 +1,44 @@
 import { beforeEach, describe, vi, it, expect } from "vitest"
 import { BaseIcon } from "../base"
-import { readFileSync } from "fs"
-import { join } from 'path';
 
 vi.mock("fs", () => {
   return {
-    readFileSync: vi.fn(() => JSON.stringify({
-      tagName: 'svg',
-      attributes: {
-        width: '24',
-        height: '24',
-        viewBox: '0 0 24 24',
-        fill: 'none',
-        xmlns: 'http://www.w3.org/2000/svg'
-      },
-      children: [{
-        tagName: 'path',
-        attributes: {
-          d: 'M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2Z',
-          fill: '#212121'
-        }
-      }]
-    }))
+    readFileSync: vi.fn((path: string) => {
+      if (path.endsWith('/json/activity-log.json')) {
+        return JSON.stringify({
+          "tagName": "svg",
+          "type": "element",
+          "attributes": {
+            "xmlns": "http://www.w3.org/2000/svg",
+            "width": "15",
+            "height": "15",
+            "fill": "none"
+          },
+          "children": [
+            {
+              "tagName": "path",
+              "type": "element",
+              "attributes": {
+                "fill": "currentColor",
+                "fill-rule": "evenodd",
+                "d": "M.877 7.5a6.623 6.623 0 1 1 13.246 0 6.623 6.623 0 0 1-13.246 0M7.5 1.827a5.673 5.673 0 1 0 0 11.346 5.673 5.673 0 0 0 0-11.346M7.125 9c-.055.127-.793 2.96-.793 2.96a.5.5 0 1 1-.966-.26s.88-2.827.88-3.43V6.801l-1.958-.525a.5.5 0 1 1 .258-.966s1.654.563 2.3.563h1.309c.645 0 2.298-.563 2.298-.563a.5.5 0 1 1 .26.966l-1.966.527V8.27c0 .603.88 3.427.88 3.427a.5.5 0 0 1-.966.259S7.92 9.127 7.869 9c-.05-.127-.219-.127-.219-.127h-.307s-.173 0-.218.127M7.5 5.12a1.125 1.125 0 1 0 0-2.25 1.125 1.125 0 0 0 0 2.25",
+                "clip-rule": "evenodd"
+              },
+              "children": []
+            }
+          ]
+        })
+      }
+      throw new Error(`Unexpected file: ${path}`)
+    })
   }
 })
 
 vi.mock("path", () => {
   return {
-    join: vi.fn((...args) => args.join('/'))
+    join: vi.fn((...args) => {
+      return args.join('/').replace(/\/+/g, '/');
+    }),
   }
 })
 
@@ -36,8 +47,9 @@ describe("baseIcon component", () => {
     vi.clearAllMocks()
   })
   it('should render an SVG string', () => {
-    const icon = new BaseIcon({ name: 'test-icon' });
+    const icon = new BaseIcon({ name: 'activity-log' });
     const result = icon.render();
+    console.log('result =>', result);
     expect(result).toContain('<svg');
     expect(result).toContain('<path');
   });
